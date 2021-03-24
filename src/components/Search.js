882292cf9c8014929164541f1792e3cbfb4594e6
David Castillo <dcastillo.st@gmail.com>
2021-03-24 17:28:05 +0900

    Add API request and display list of results

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Search = () => {
-  const [term, setTerm] = useState("")
+  const [term, setTerm] = useState('Programming');
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    const search = async () => {
+      const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: term,
+        }
+      });
+
+      setResults(data.query.search);
+    };
+
+    search();
+  }, [term]);
+
+  const renderedResults = results.map((result) => {
+    return(
+      <div key={result.pageid} className="item">
+        <div className="content">
+          <div className="header">
+            {result.title}
+          </div>
+          {result.snippet}
+        </div>
+      </div>
+    );
+  });
 
   return (
     <div>
@@ -14,6 +47,9 @@ const Search = () => {
             className="input"
           />
         </div>
+      </div>
+      <div className="ui celled list">
+        {renderedResults}
       </div>
     </div>
   );
