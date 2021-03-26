f7e2d3312b44021d23a87b9321559bd2422b26c2
David Castillo <dcastillo.st@gmail.com>
2021-03-26 17:04:11 +0900

    Fix React warning by using debounced term

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,7 +3,18 @@ import axios from 'axios';
 
 const Search = () => {
   const [term, setTerm] = useState('Programming');
+  const [debouncedTerm, setDebouncedTerm] = useState(term);
   const [results, setResults] = useState([]);
+  const timeout = 500;
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedTerm(term);
+    }, timeout);
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [term]);
 
   useEffect(() => {
     const search = async () => {
@@ -13,25 +24,15 @@ const Search = () => {
           list: 'search',
           origin: '*',
           format: 'json',
-          srsearch: term,
+          srsearch: debouncedTerm,
         }
       });
 
       setResults(data.query.search);
     };
 
-    if (term && !results.length) {
-      search();
-    } else {
-      const timeoutId = setTimeout(() => {
-        if (term) search();
-      }, 500);
-
-      return () => {
-        clearTimeout(timeoutId);
-      };
-    }
-  }, [term]);
+    if(term) search();
+  }, [debouncedTerm]);
 
   const renderedResults = results.map((result) => {
     return (
